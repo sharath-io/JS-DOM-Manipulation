ea12565aea83cbfa7c32acdb01107e91a22dc01e
Sharath <sharath.esr@gmail.com>
2025-01-15 21:23:06 +0530

    add event propogation

---
+++ eventPropogation/eventPropogation.js
@@ -0,0 +1,28 @@
+// Event Propogation
+
+window.addEventListener('click', function(){
+    console.log('window')
+},false);
+
+
+document.addEventListener('click', function(){
+    console.log('document');
+},false);
+
+
+document.querySelector('.div2').addEventListener('click', function(e){
+    console.log('DIV 2');
+},{once:true});
+
+
+document.querySelector('.div1').addEventListener('click', function(){
+    console.log('DIV 1');
+},false);
+
+
+document.querySelector('.button').addEventListener('click', function(e){
+    e.preventDefault();
+    console.log(e.target.innerText = 'Clicked')
+},false);
+
+
